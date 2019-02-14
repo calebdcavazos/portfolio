571cc883e9ee6c541f5ebad15af16caa7c9f13a3
Caleb <calebdcavazos@gmail.com>
2019-02-13 23:26:28 -0500

    worked on the gulpfile and got all gulp tasks working properly. Began to set up postcss as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,47 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+//Require in Gulp and other dependencies
+const gulp = require('gulp'),
+postcss = require('gulp-postcss');
 
-gulp.task('default', function() {
-    console.log('Hooray, you created a gulp task');
-});
 
-gulp.task('html', function() {
-    console.log('Imagine something useful being done to your html here');
-});
+// define often reused variables
+var index = "./app/index.html";
+var all_styles = "./app/assets/styles/**/*.css";
+var main_style = "./app/assets/styles/style.css";
+var temp_styles = './app/temp/styles';
 
-gulp.task('watch', function() {
-    watch('./app/index.html', function() {
-        gulp.start('html');
-    });
-});
+
+//create all functions for gulp to run
+function html(done) {
+    console.log('Html task is running');
+    done();
+}
+
+function js(done) {
+    console.log('JS task is running');
+    done();
+}
+
+function css(done) {
+    gulp.src(main_style)
+    .pipe()
+    .pipe(gulp.dest(temp_styles));
+    done();
+}
+
+function watch_index() {
+    gulp.watch(index, html);
+}
+
+function watch_css() {
+    gulp.watch(all_styles, css);
+}
+
+
+//define gulp tasks
+gulp.task('html', html);
+gulp.task('js', js);
+gulp.task('css', css);
+gulp.task('default', gulp.parallel(html, js));
+gulp.task('watch', gulp.parallel(watch_index, watch_css));
+
+
