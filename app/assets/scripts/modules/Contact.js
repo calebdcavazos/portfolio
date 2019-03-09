0ccfb344b2e442a1f679dc5269b6829feba771d4
Caleb <calebdcavazos@gmail.com>
2019-03-09 11:18:29 -0500

    finished the javascript for the contact modal. and averted disaster after finding missplaced semi-colon

--- app/assets/scripts/modules/Contact.js
+++ app/assets/scripts/modules/Contact.js
@@ -4,15 +4,17 @@ class Contact {
     constructor() {
         this.contact = $('.contact');
         this.contactBtn = $('.main-menu__contact-btn');
+        this.contactClose = $('.contact__close');
         this.events();
     }
 
     events() {
-        this.contactBtn.click(toggleTheModal.bind(this));
+        this.contactBtn.click(this.toggleTheModal.bind(this));
+        this.contactClose.click(this.toggleTheModal.bind(this));
     }
 
     toggleTheModal() {
-        this.contact.toggleClass('.contact--is-visible');
+        this.contact.toggleClass('contact--is-visible');
     }
 }
 
