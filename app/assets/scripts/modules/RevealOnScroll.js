c3227fa968dbe55611251a9e39c1cfb89f025167
Caleb <calebdcavazos@gmail.com>
2019-03-11 22:59:52 -0400

    finished the js for smooth-scroll and item reveal. Also began working on support for legacy browsers. Almost finish with build and almost ready to move on to actual content

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,8 +1,29 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
     constructor() {
-        this.itemsToReveal = $(".about");
+        this.itemsToReveal = $(".about, .portfolio");
+        this.hideInitially();
+        this.createWaypoints();
+    }
+
+    hideInitially() {
+        this.itemsToReveal.addClass("reveal-item");
+    }
+
+    createWaypoints() {
+        this.itemsToReveal.each(function() {
+            let currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function() {
+                    $(currentItem).addClass("reveal-item--is-visible");
+                },
+                offset: "50%"
+
+            });
+        });
     }
 }
 
--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,9 +1,50 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
     constructor() {
         this.siteHeader = $(".site-header");
+        this.pageSections = $(".page-section");
+        this.headerLinks = $(".page-link");
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
     }
+
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
+    }
+
+    createPageSectionWaypoints() {
+        let that = this;
+        this.pageSections.each(function() {
+            let currentPageSection = this;
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction =="down") {
+                        let matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "18%"
+            });
+
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction =="up") {
+                        let matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "-40%"
+            });
+        });
+    }
+
 }
 
 export default StickyHeader;
